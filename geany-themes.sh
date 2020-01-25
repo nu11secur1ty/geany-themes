5189de303c25d9d13d7a1b5a089bff0055fc9ddc
nu11secur1ty <venvaropt@gmail.com>
2020-01-24 21:17:33 -0500

    Update geany-themes.sh

--- geany-themes.sh
+++ geany-themes.sh
@@ -10,15 +10,7 @@ git clone https://github.com/geany/geany-themes.git
   cd geany-themes
 
 # install themes
-CSDIR="$HOME/.config/geany/colorschemes/"
-echo "Installing themes into \`$CSDIR'..."
-mkdir -p "$CSDIR"
-for SCHEME in `ls colorschemes/*.conf`
-do
-  BNAME=`basename "$SCHEME"`
-  echo " => $BNAME"
-  cp "$SCHEME" "$CSDIR"
-done
+./install.sh
   cd 
     rm -rf geany-themes
 exit 0;
