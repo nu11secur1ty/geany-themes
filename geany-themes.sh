67ce7cffc10489daf45adc1774ebe73e671a30a3
nu11secur1ty <venvaropt@gmail.com>
2020-01-24 21:11:28 -0500

    Update geany-themes.sh

--- geany-themes.sh
+++ geany-themes.sh
@@ -1,6 +1,9 @@
 #!/usr/bin/bash
 # by nu11secur1ty
 # install geany
+apt purge geany -y
+cd /root/.config
+rm -rf geany
 apt install -y geany geany-plugins
 # get themes
 git clone https://github.com/geany/geany-themes.git
